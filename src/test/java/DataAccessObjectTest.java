c6a44ce332ac7bc2dedf8b819a6c1191fe2e5ed0
Claus Gumbmann <claus.gumbmann@schule.bayern.de>
2024-11-01 15:36:10 +0100

    Update Tests

--- src/test/java/DataAccessObjectTest.java
+++ src/test/java/DataAccessObjectTest.java
@@ -1,4 +1,6 @@
 import models.Article;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import services.DataAccessObject;
 
@@ -9,10 +11,23 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class DataAccessObjectTest {
 
+	private static List<Article> articles;
+
+	@BeforeAll
+	public static void setUp() {
+		DataAccessObject dao = new DataAccessObject();
+		articles = dao.findAll();
+	}
+
 	@Test
-	public void testFindAll() {
-		List<Article> articles = DataAccessObject.findAll();
+	@DisplayName("FindAll should return a non-null list of articles")
+	public void testFindAllReturnsNonNullList() {
 		assertNotNull(articles, "The list of articles should not be null");
+	}
+
+	@Test
+	@DisplayName("List of Articles should have 500 Elements")
+	public void testListOfArticlesHasSize500() {
 		assertEquals(500, articles.size(), "The count of items should be 500");
 	}
 }
